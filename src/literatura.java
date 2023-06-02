71f13207cb1b2beb8093de26f57ec436bc92e1e8
Juan Falconi <juan.falconi@epn.edu.ec>
2023-06-02 12:27:55 -0500

    Prueba1

--- src/literatura.java
+++ src/literatura.java
@@ -1,2 +1,71 @@
-package PACKAGE_NAME;public class literatura {
+public class literatura {
+    //Atributos
+    String autor; // variable en donde se almacenara el autor del libro tipo cadena
+    int anio; // varible en donde se almacenara el año del libro entero
+    String titulo; // variable donde se alamacenara el titulo del libro de tipo cadena
+    int nPage; // variable donde se alamacenara el numero de paginas del libro de tipo entero
+    String edicion; //variable donde se almacenara la edicion del libro de dato tipo cadena
+
+    public literatura(String autor, int anio, String titulo, int nPage, String edicion) { // usando el constructor para inicializar los atributos
+        this.autor = autor;
+        this.anio = anio;
+        this.titulo = titulo;
+        this.nPage = nPage;
+        this.edicion = edicion;
+    }
+
+    //creacion de los metodos setters and getters para usarlos mas adelanta en el main
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getnPage() {
+        return nPage;
+    }
+
+    public void setnPage(int nPage) {
+        this.nPage = nPage;
+    }
+
+    public String getEdicion() {
+        return edicion;
+    }
+
+    public void setEdicion(String edicion) {
+        this.edicion = edicion;
+    }
+
+    //creamor un metodo para mostrar la informacion del libro
+    public void imprimirLibros(){
+        System.out.print("\nNombre del autor: ");
+        System.out.println(this.getAutor());
+        System.out.print("El año del lanzamiento: ");
+        System.out.println(this.getAnio());
+        System.out.print("Titulo del Libro: ");
+        System.out.println(this.getTitulo());
+        System.out.print("Paginas totales del libro: ");
+        System.out.println(this.getnPage());
+        System.out.print("Edicion: ");
+        System.out.println(this.getEdicion());
+    }
 }
--- src/main.java
+++ src/main.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class main {
+public class main {
+    public static void main(String[] args) {
+
+        // creamos la instancia para los libros con el constructor de la clase literatura
+        literatura libro1 = new literatura("Juan Falconi",2002,"Aventuras del Bosque",189,"Primera");
+        literatura libro2 = new literatura("Liam Loor",2015,"Amigos Inseparables",200,"Tercera");
+        literatura libro3 = new literatura("Kerwin Peñafiel",2020,"¿Como ganar amigos?",320,"Segunda");
+        literatura libro4 = new literatura("Joselin Jaramillo",2004,"Aventuras de Amor con Joss",250,"Cuarta");
+        literatura libro5 = new literatura("Neithan Boorges",2023,"La musica salva vidas",130,"Primera");
+
+        //Libro 1
+        libro1.imprimirLibros();
+        libro2.imprimirLibros();
+        libro3.imprimirLibros();
+        libro4.imprimirLibros();
+        libro5.imprimirLibros();
+
+    }
 }
